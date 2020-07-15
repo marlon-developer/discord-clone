a44a7e83df056d991fe1effea857ac67044ba61d
Marlon da Rosa <marlonti17@gmail.com>
2020-07-14 23:55:02 -0300

    Add Channel Info

---
+++ src/components/ChannelInfo/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Container, HashtagIcon, Title, Separator, Description } from './styles';
+
+const ChannelInfo: React.FC = () => {
+    return (
+        <Container>
+            <HashtagIcon />
+
+            <Title># bate-papo</Title>
+
+            <Separator />
+
+            <Description>Canal Aberto Para Conversas</Description>
+        </Container >
+    );
+};
+
+export default ChannelInfo;
+
