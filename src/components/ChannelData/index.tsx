bbf0b8833b004289185f217319fa087ae4c4f1e3
Marlon da Rosa <marlonti17@gmail.com>
2020-07-15 19:57:29 -0300

    Add Channel Messages

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+
+import ChannelMessage, { Mention } from '../ChannelMessage';
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
 
 const ChannelData: React.FC = () => {
+    const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+    useEffect(() => {
+        const div = messagesRef.current;
+
+        if (div) {
+            div.scrollTop = div.scrollHeight;
+        }
+    }, [messagesRef]);
+
     return (
         <Container>
-            <Messages />
+            <Messages ref={ messagesRef }>
+                {Array.from(Array(15).keys()).map((n) => (
+                <ChannelMessage key={n} author="Ghost" date="15/07/2020" content="Isso é uma Mensagem Enviada!"/>))}
+
+                <ChannelMessage author="Usuário 1" date="21/06/2020" content={<><Mention>@Ghost</Mention> me invita aew no 7 days to die?</>} hasMention isBot />
+            </Messages>
+
+            
 
             <InputWrapper>
                 <Input type="text" placeholder="Escreva uma mensagem ..." />
